07b7f23a1ec70abbb59b5aa77b490423ef03f93b
sanjeev <sanjeevsanjeevbaghel@gmail.com>
2020-05-23 16:16:54 +0530

    publish

--- BookPlace/src/environments/environment.prod.ts
+++ BookPlace/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://bookplace-4ca58.firebaseio.com/',
+  apiKey: 'AIzaSyClp3tgP07CK8nzGjFWNLlB7Gu2FRAbTzE',
+  mapbox: {
+    accessToken:
+      'pk.eyJ1Ijoic2FuYmFnaCIsImEiOiJjazdnaWZ3MnkwMDFzM2xwODE4ZGZ2NTljIn0.N_4NpltJgDFzZbRGT_1Pwg',
+  },
 };
